aa5b602d10ac3d5bd5868cb53705a1ff923fa546
Qlevine <Qlevine20@yahoo.com>
2022-04-29 20:54:21 -0700

    Create a shader that inverts the colors of everything in view of the camera

---
+++ Assets/PostEffect.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostEffect : MonoBehaviour
+{
+    public Material postEffectMat;
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source,destination,postEffectMat);
+    }
+}
